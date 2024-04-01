2e6f19c8b4a22c69092ac96db0813d1fcd14817b
Igor <qwerty2244668800@gmail.com>
2024-04-01 14:37:27 +0300

    image carousel css height styles are improved,mobile markup for SingleDevicePage is improved and finished

--- src/pages/SingleDevicePage.jsx
+++ src/pages/SingleDevicePage.jsx
@@ -19,6 +19,8 @@ import {
 } from "@mui/material";
 import ImageGallery from "react-image-gallery";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import BalanceIcon from "@mui/icons-material/Balance";
 import "react-image-gallery/styles/css/image-gallery.css";
 
 import img from "../images/iphone_15/iphone_15_pro_max_natural_titanium_pdp_image_position-1__ww-en_1.jpeg";
@@ -29,11 +31,11 @@ import img4 from "../images/iphone_15/iphone_15_pro_max_natural_titanium_pdp_ima
 import img5 from "../images/iphone_15/iphone_15_pro_max_natural_titanium_pdp_image_position-4__ww-en_1.jpeg";
 
 const images = [
-    { original: img1, thumbnail: img1, originalHeight: "500px" },
-    { original: img2, thumbnail: img2, originalHeight: "500px" },
-    { original: img3, thumbnail: img3, originalHeight: "500px" },
-    { original: img4, thumbnail: img4, originalHeight: "500px" },
-    { original: img5, thumbnail: img5, originalHeight: "500px" },
+    { original: img1, thumbnail: img1 },
+    { original: img2, thumbnail: img2 },
+    { original: img3, thumbnail: img3 },
+    { original: img4, thumbnail: img4 },
+    { original: img5, thumbnail: img5 },
 ];
 
 const RoundButton = styled(Button)(({ back }) => ({
@@ -44,7 +46,6 @@ const RoundButton = styled(Button)(({ back }) => ({
     padding: 0,
     marginRight: "8px",
     opacity: 0.9,
-    // backgroundColor: `${back}`,
     backgroundColor: back,
     "&:hover": {
         border: "3px solid #BDBDBD",
@@ -53,11 +54,28 @@ const RoundButton = styled(Button)(({ back }) => ({
     },
 }));
 
+const CardContentCustomized = styled(CardContent)(({ theme, needmobilechange = "true" }) => ({
+    display: "flex",
+    flexDirection: "row",
+    // flexDirection: "column", mobile
+    alignItems: "center",
+    backgroundColor: "white",
+    padding: 8,
+    // paddingBottom: 0,
+    "&:last-child": {
+        padding: 8,
+    },
+    [theme.breakpoints.down("sm")]: {
+        flexDirection: needmobilechange === "true" ? "column" : "row",
+    },
+}));
+
 const SingleDevicePage = () => {
     const theme = useTheme();
     const isThinnerThanLarge = useMediaQuery(theme.breakpoints.down("lg"));
     return (
         <Grid container spacing={1}>
+            {/* ---Breadcrumb start--- */}
             <Grid item xs={12}>
                 <Stack>
                     <Breadcrumbs aria-label="breadcrumb">
@@ -71,29 +89,42 @@ const SingleDevicePage = () => {
                     </Breadcrumbs>
                 </Stack>
             </Grid>
+            {/* ---Breadcrumb finish--- */}
             <Grid item xs={12} lg={5}>
-                <Card elevation={2} sx={{ p: 2 }} square={true}>
+                {isThinnerThanLarge && (
+                    <Card elevation={2} sx={{ p: 1, pl: 3, pr: 3, mb: 1 }} square={true}>
+                        <CardContentCustomized>
+                            <Typography component="h4" variant="h4">
+                                iPhone 14 128GB Red
+                            </Typography>
+                        </CardContentCustomized>
+                    </Card>
+                )}
+                <Card elevation={2} sx={{ p: 2, height: "450px" }} square={true}>
                     <ImageGallery
                         items={images}
                         showNav={true}
                         showPlayButton={false}
                         showFullscreenButton={true}
                         useBrowserFullscreen={false}
+                        showThumbnails={true}
                     />
                 </Card>
             </Grid>
             <Grid item container xs={12} lg={7}>
-                <Grid item lg={12}>
-                    <Card elevation={2} sx={{ p: 2, mb: 1 }} square={true}>
-                        {/* <CardContent sx={{ p: 1, pb: 0 }}> */}
-                        <Typography component="h4" variant="h4">
-                            iPhone 14 128GB Red
-                        </Typography>
-                        {/* </CardContent> */}
-                    </Card>
-                    <Card elevation={2} sx={{ p: 2, mb: 1 }} square={true}>
-                        <CardContent sx={{ p: 1, pb: 0 }}>
-                            <Typography component="h6" variant="h6">
+                <Grid item xs={12}>
+                    {!isThinnerThanLarge && (
+                        <Card elevation={2} sx={{ p: 1, pl: 3, pr: 3, mb: 1 }} square={true}>
+                            <CardContentCustomized>
+                                <Typography component="h4" variant="h4">
+                                    iPhone 14 128GB Red
+                                </Typography>
+                            </CardContentCustomized>
+                        </Card>
+                    )}
+                    <Card elevation={2} sx={{ p: 1, pl: 3, pr: 3, mb: 1 }} square={true}>
+                        <CardContentCustomized>
+                            <Typography component="h6" variant="h6" sx={{ mr: 2 }}>
                                 Storage options:
                             </Typography>
                             <ButtonGroup variant="outlined" aria-label="Basic button group">
@@ -101,36 +132,49 @@ const SingleDevicePage = () => {
                                 <Button>512GB</Button>
                                 <Button>1024GB</Button>
                             </ButtonGroup>
-                        </CardContent>
+                        </CardContentCustomized>
                     </Card>
-                    <Card elevation={2} sx={{ p: 2, mb: 1 }} square={true}>
-                        <CardContent sx={{ p: 1, pb: 0 }}>
-                            <Typography component="h6" variant="h6">
+                    <Card elevation={2} sx={{ p: 1, pl: 3, pr: 3, mb: 1 }} square={true}>
+                        <CardContentCustomized>
+                            <Typography component="h6" variant="h6" sx={{ mr: 2 }}>
                                 Colors:
                             </Typography>
-                            <RoundButton back="red"></RoundButton>
-                            <RoundButton back="black"></RoundButton>
-                            <RoundButton back="yellow"></RoundButton>
-                        </CardContent>
+                            <div>
+                                <RoundButton back="red"></RoundButton>
+                                <RoundButton back="black"></RoundButton>
+                                <RoundButton back="yellow"></RoundButton>
+                            </div>
+                        </CardContentCustomized>
                     </Card>
-                    <Card elevation={2} sx={{ p: 2, mb: 1 }} square={true}>
-                        <CardContent sx={{ p: 1, pb: 0, display: "flex", alignItems: "center", gap: 1 }}>
-                            <Typography component="h4" variant="h4" mr={4}>
-                                900 $
+                    <Card elevation={2} sx={{ p: 1, pl: 3, pr: 3, mb: 1 }} square={true}>
+                        <CardContentCustomized needmobilechange="false" sx={{ justifyContent: "space-between", gap: 1 }}>
+                            <Typography component="h4" variant="h4">
+                                900$
                             </Typography>
-                            <Button variant="contained" size="large">
-                                Buy
-                            </Button>
-                            <IconButton aria-label="add to favorites" size="large">
-                                <FavoriteIcon />
-                            </IconButton>
-                        </CardContent>
+
+                            <div>
+                                <IconButton aria-label="add to favorites" size="large">
+                                    <FavoriteIcon />
+                                </IconButton>
+                                <IconButton aria-label="add to commparison" size="large">
+                                    <BalanceIcon />
+                                </IconButton>
+                                <IconButton aria-label="share" size="large">
+                                    <ShareIcon />
+                                </IconButton>
+                            </div>
+                        </CardContentCustomized>
                         {/* <CardActions sx={{ p: 1, justifyContent: "flex-start" }}>
                             <Button variant="contained">Buy</Button>
                             <IconButton aria-label="add to favorites">
                                 <FavoriteIcon />
                             </IconButton>
                         </CardActions> */}
+                    </Card>
+                    <Card elevation={0}>
+                        <Button variant="contained" size="large" sx={{ width: "100%" }}>
+                            Add to Cart
+                        </Button>
                     </Card>
                 </Grid>
             </Grid>
