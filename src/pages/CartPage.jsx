a725e42f48c7e15370770e1dbe0fe2b279564fb5
Igor <qwerty2244668800@gmail.com>
2024-04-01 17:31:00 +0300

    CartItem style is implemented

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -21,35 +21,51 @@ const CartPage = () => {
     return (
         <Grid container spacing={1}>
             <Grid className="cart-items" item xs={12} lg={8}>
-                <Card elevation={2} sx={{ p: 2 }}>
-                    <CardActionArea className="my-card-action-area" to="/devices/123">
-                        <CardMedia sx={{ height: 150, objectFit: "contain" }} image={iphone15Img} title="stuff" component="img" />
-                    </CardActionArea>
-                    <CardContent sx={{ p: 1 }}>
+                <Card elevation={2} sx={{ p: 2, display: "flex" }}>
+                    <CardContent sx={{ p: 0, flexGrow: 1 }}>
+                        <CardActionArea className="my-card-action-area" to="/devices/123">
+                            <CardMedia sx={{ height: 100, objectFit: "contain" }} image={iphone15Img} title="stuff" component="img" />
+                        </CardActionArea>
+                    </CardContent>
+
+                    <CardContent sx={{ p: 0, flexGrow: 4, display: "flex", flexDirection: "column" }}>
                         <Link to="/devices/123">
-                            <Typography component="h6" variant="subtitle1">
+                            <Typography component="h6" variant="h6">
                                 iPhone 14 128GB Red
                             </Typography>
                         </Link>
-                        <Typography component="p" variant="body1">
+                        <Typography component="p" variant="body2">
                             Quickfind Code: 1828880
                         </Typography>
-                        <Box>
-                            <IconButton aria-label="add to favorites">
-                                <FavoriteIcon />
-                            </IconButton>
-                            <IconButton aria-label="delete from cart">
-                                <DeleteIcon />
-                            </IconButton>
+                        <Box sx={{ mt: "auto" }}>
+                            <Button variant="text" sx={{ textTransform: "none", mr: 1 }} size="small" startIcon={<FavoriteIcon />}>
+                                Add to Favorite
+                            </Button>
+                            <Button variant="text" sx={{ textTransform: "none" }} size="small" startIcon={<DeleteIcon />}>
+                                Delete item
+                            </Button>
                         </Box>
                     </CardContent>
-                    <CardContent sx={{ p: 1 }}>
+
+                    <CardContent
+                        sx={{
+                            p: 0,
+                            flexGrow: 1,
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                            "&:last-child": {
+                                pb: 0,
+                            },
+                        }}
+                    >
                         <Typography component="h5" variant="h5">
                             900 $
                         </Typography>
                         <ButtonGroup variant="outlined" aria-label="Basic button group">
                             <Button>-</Button>
-                            <Button>1</Button>
+                            <Button disableRipple>1</Button>
                             <Button>+</Button>
                         </ButtonGroup>
                     </CardContent>
